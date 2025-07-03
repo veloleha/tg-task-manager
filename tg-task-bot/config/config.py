8864c68b9357175ca6766147da519fb4d35d6197
veloleha <veloleha@gmail.com>
2025-07-03 23:19:36 +0300

    Update config.py

---
+++ tg-task-bot/config/config.py
@@ -0,0 +1,7 @@
+# config.py
+class Settings:
+    REDIS_HOST = "localhost"  # Адрес хоста Redis
+    REDIS_PASSWORD = "748596lPNH!"  # Пароль (если требуется)
+    # Другие настройки, если нужны
+
+settings = Settings()
